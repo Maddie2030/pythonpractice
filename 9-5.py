ce5ff0ef8c39286971e97d5417cc2ac8b65a85b0
Maddie2030 <madhurian2030@gmail.com>
2024-05-18 16:25:36 +0530

    meg

--- 9-5.py
+++ 9-5.py
@@ -1,7 +1,23 @@
-# sum of elements in a list
+####### sum of elements in a list
+
+# l = [4,7,8,4,5,3,5]
+# sum=0
+# for i in l:
+#    sum= sum+i
+# print(sum)
+
+###### Multiplication of elements in a list
+
+# l = [4,7,8,4,5,3,5]
+# mult=1
+# for i in l:
+#   mult=mult*i
+# print(mult)
+
+###### count of all the elements in a list
 
 l = [4,7,8,4,5,3,5]
-sum=0
+count = 0
 for i in l:
-   sum= sum+i
-print(sum)
+  count+=1
+print(count)
