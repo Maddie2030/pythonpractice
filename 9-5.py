f71564a7c86d22c2c6d47ce5d9f2085d1ed34167
Maddie2030 <madhurian2030@gmail.com>
2024-05-18 18:15:12 +0530

    t

--- 9-5.py
+++ 9-5.py
@@ -16,8 +16,27 @@
 
 ###### count of all the elements in a list
 
-l = [4,7,8,4,5,3,5]
-count = 0
-for i in l:
-  count+=1
-print(count)
+# l = [4,7,8,4,5,3,5]
+# count = 0
+# for i in l:
+#   count+=1
+# print(count)
+
+###### swapping the first and last elements of a list
+
+# l = [4,7,8,4,5,3,5]
+# k=l[0]
+# l[0]=l[-1]
+# l[-1]=k
+# print(l)
+
+##### removing of elements from a list
+
+# l = [4,7,8,4,5,3,5]
+# k=int(input("enter the number to remove"))
+# for i in l:
+#   if i==k:
+#     l.remove(i)
+#     break
+# print(l)
+
